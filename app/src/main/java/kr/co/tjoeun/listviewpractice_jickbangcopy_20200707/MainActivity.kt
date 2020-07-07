6fea2b8e644e9cf004a0cecb18ef450c7a556034
김재영 <kjymonster@naver.com>
2020-07-07 21:50:20 +0900

    연습용 방 목록 직접 추가

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -2,12 +2,13 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.adapters.RoomAdapter
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
 
 class MainActivity : BaseActivity() {
 
-    val rRoomList = ArrayList<Room>()
+    val mRoomList = ArrayList<Room>()
 
     lateinit var mAdater: RoomAdapter
 
@@ -18,7 +19,13 @@ class MainActivity : BaseActivity() {
         setupEvents()
         setValues()
 
-        mAdater =
+        mAdater = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+
+        roomListView.adapter = mAdater
+
+
+
+
     }
 
 
@@ -28,6 +35,18 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        mRoomList.add(Room(8000, "마포구 서교동", 1, "망원/홍대역 풀옵션 넓은 원룸"  ))
+        mRoomList.add(Room(28500,"마포구 서교동",3,"신혼부부의 보금자리 서교동 신축빌라"))
+        mRoomList.add(Room(30000, "마포구 서교동", 10, "망원/홍대역 풀옵션 넓은 원룸"  ))
+        mRoomList.add(Room(24500,"마포구 서교동",13,"신혼부부의 보금자리 서교동 신축빌라"))
+        mRoomList.add(Room(9000, "은평구 불광동", 5, "연신내 풀옵션 넓은 원룸"  ))
+        mRoomList.add(Room(12400,"은평구 불광동",6,"불광역 도보1분 보금자리 서교동 신축빌라"))
+        mRoomList.add(Room(55000, "동대문구 OO동", 8, "롯데백화점 근접 풀옵션 넓은 원룸"  ))
+        mRoomList.add(Room(52300,"은평구 녹번동",12,"신혼부부의 보금자리 녹번동 신축빌라"))
+    
+    
+    
+    
     }
 
 
