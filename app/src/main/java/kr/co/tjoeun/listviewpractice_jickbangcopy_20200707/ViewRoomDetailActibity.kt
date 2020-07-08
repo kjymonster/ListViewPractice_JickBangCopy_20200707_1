9ed4b7e7ac408903da92d2d8392576df18d19b0f
김재영 <kjymonster@naver.com>
2020-07-08 21:05:55 +0900

    icon/roundIcon을 직접만든 mipmap을 가지고 설정

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomDetailActibity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomDetailActibity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_view_room_detail_actibity.*
 import kotlinx.android.synthetic.main.room_list_item.*
 import kotlinx.android.synthetic.main.room_list_item.priceTxt
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detail_actibity.descTxt as descTxt1
 
 class ViewRoomDetailActibity : BaseActivity() {
 
@@ -27,7 +28,7 @@ class ViewRoomDetailActibity : BaseActivity() {
        val receivedRoom = intent.getSerializableExtra("room") as Room
 
         priceTxt.text = receivedRoom.getFormattedPrice()
-        commentTxt.text =receivedRoom.description
+        descTxt.text =receivedRoom.description
         addressTxt.text = receivedRoom.address
         floorTxt.text = receivedRoom.getFormattedFloor()
 
