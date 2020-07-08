c53b77b8867a9877b381a48f175e2047a5399f4f
김재영 <kjymonster@naver.com>
2020-07-08 20:07:47 +0900

    방 상세화면을 추가

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.adapters.RoomAdapter
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
@@ -23,25 +24,33 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        roomListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedRoom = mRoomList[position]
+
+            //눌린 방의 정보를 상세화면 Activity에 전달
+
+            // Toast.makeText(this,clickedRoom, Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun setValues() {
 
-        mRoomList.add(Room(8000, "마포구 서교동", 1, "망원/홍대역 풀옵션 넓은 원룸"  ))
-        mRoomList.add(Room(28500,"마포구 서교동",3,"신혼부부의 보금자리 서교동 신축빌라"))
-        mRoomList.add(Room(30000, "마포구 서교동", 10, "망원/홍대역 풀옵션 넓은 원룸"  ))
-        mRoomList.add(Room(24500,"마포구 서교동",13,"신혼부부의 보금자리 서교동 신축빌라"))
-        mRoomList.add(Room(9000, "은평구 불광동", 5, "연신내 풀옵션 넓은 원룸"  ))
-        mRoomList.add(Room(12400,"은평구 불광동",-1,"불광역 도보1분 보금자리 서교동 신축빌라"))
-        mRoomList.add(Room(55000, "동대문구 OO동", 0, "롯데백화점 근접 풀옵션 넓은 원룸"  ))
-        mRoomList.add(Room(52300,"은평구 녹번동",12,"신혼부부의 보금자리 녹번동 신축빌라"))
+        mRoomList.add(Room(8000, "마포구 서교동", 1, "망원/홍대역 풀옵션 넓은 원룸"))
+        mRoomList.add(Room(28500, "마포구 서교동", 3, "신혼부부의 보금자리 서교동 신축빌라"))
+        mRoomList.add(Room(30000, "마포구 서교동", 10, "망원/홍대역 풀옵션 넓은 원룸"))
+        mRoomList.add(Room(24500, "마포구 서교동", 13, "신혼부부의 보금자리 서교동 신축빌라"))
+        mRoomList.add(Room(9000, "은평구 불광동", 5, "연신내 풀옵션 넓은 원룸"))
+        mRoomList.add(Room(12400, "은평구 불광동", -1, "불광역 도보1분 보금자리 서교동 신축빌라"))
+        mRoomList.add(Room(55000, "동대문구 OO동", 0, "롯데백화점 근접 풀옵션 넓은 원룸"))
+        mRoomList.add(Room(52300, "은평구 녹번동", 12, "신혼부부의 보금자리 녹번동 신축빌라"))
 
         mRoomAdater = RoomAdapter(mContext, R.layout.room_list_item, mRoomList)
         roomListView.adapter = mRoomAdater
-    
+
 
     }
-
 
 
 }
---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomDetailActibity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActibity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail_actibity)
+    }
+}
